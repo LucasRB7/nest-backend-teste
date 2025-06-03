b2b140a41277df1d8b59497a232eac9d1a71a28b
LucasRB7 <lucasrb.dev@gmail.com>
2025-06-03 15:18:12 -0300

    port dinamica web-socket

--- src/notifications/notifications.gateway.ts
+++ src/notifications/notifications.gateway.ts
@@ -1,7 +1,7 @@
 import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway(3001,{
+@WebSocketGateway({
   cors: {
     origin: '*',
   },
