30be14e1d4f2873b685b1588c91ab5cb20caf81f
LucasRB7 <lucasrb.dev@gmail.com>
2025-06-03 08:53:25 -0300

    "new version"

--- src/skins/skins.controller.ts
+++ src/skins/skins.controller.ts
@@ -6,15 +6,8 @@
       Param,
       Patch,
       Post,
-      UploadedFile,
       UseGuards,
-      UseInterceptors
     } from '@nestjs/common';
-    import { FileInterceptor } from '@nestjs/platform-express';
-    import { storage } from './firebase'; // Supondo que você exportou storage aqui
-    import * as multer from 'multer';
-    import { v4 as uuid } from 'uuid';
-    import * as path from 'path';
 import { SkinsService } from './skins.service';
 import { ApiOperation } from '@nestjs/swagger';
 import { NotificationsGateway } from 'src/notifications/notifications.gateway';
@@ -61,38 +54,4 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
       ){
         return this.Skins.update(id, Body.url, Body.comp);
       }
-
-      @Post('upload')
-      @UseInterceptors(FileInterceptor('file', {
-        storage: multer.memoryStorage(),
-        limits: { fileSize: 5 * 1024 * 1024 },
-      }))
-      async uploadSkin(@UploadedFile() file: Express.Multer.File) {
-        const bucket = storage.bucket();
-        const filename = `${uuid()}${path.extname(file.originalname)}`;
-        const filePath = `skins/${filename}`;
-        const blob = bucket.file(filePath);
-    
-        const stream = blob.createWriteStream({
-          metadata: {
-            contentType: file.mimetype,
-          },
-          resumable: false,
-        });
-    
-        return new Promise((resolve, reject) => {
-          stream.on('error', (err) => reject(err));
-          stream.on('finish', async () => {
-            await blob.makePublic(); // torna o arquivo público
-    
-            // ✅ Formata URL no padrão Firebase com alt=media e caminho codificado
-            const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(filePath)}?alt=media`;
-    
-            resolve({ url: publicUrl });
-          });
-    
-          stream.end(file.buffer);
-        });
-      }
     }
-    
