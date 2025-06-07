3476469f3700e275eba93a5a3d10ec2a6e99454f
LucasRB7 <lucasrb.dev@gmail.com>
2025-06-07 17:26:36 -0300

    Correção CORS ORIGIN

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useWebSocketAdapter(new IoAdapter(app));
    app.enableCors({
-        origin: "*",  // Altere para seu domínio final em produção
+        origin: "https://dlrbtech.com/sebbycomunidade/",  // Altere para seu domínio final em produção
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
         credentials: true,
         allowedHeaders: ["Content-Type", "Authorization"],
